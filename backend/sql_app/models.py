6bec1e33c22665afbd63a1b9a3666875c0a2880a
kiwavi <dekamotho@gmail.com>
2023-12-05 05:48:13 +0000

    feat: initial pydantic models, added extra fields in models.py

--- backend/sql_app/models.py
+++ backend/sql_app/models.py
@@ -2,6 +2,7 @@ from .database import Base
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship
 from sqlalchemy_imageattach.entity import Image, image_attachment
+from datetime import datetime
 
 # fields i should add are deleted_at, createdat to every table. They should be null by default because we'll be doing migrations.  
 
@@ -12,7 +13,7 @@ class User():
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
-    creation_date = Column(DateTime,nullable=True)
+    creation_date = Column(DateTime,default=datetime.now(),nullable=True)
     deletion_date = Column(DateTime, nullable=True)
     # relationships
     user_lang_id = Column(Integer,ForeignKey("languages.id"),nullable=True)
@@ -20,6 +21,7 @@ class User():
 
     user_lang = relationship("Languages", back_populates="words")
     language_fam = relationship("Language_Families", back_populates="words_fam")
+    words = relationship("Words", back_populates="user")
     
 class Words():
     __tablename__ = 'words'
@@ -27,14 +29,17 @@ class Words():
     id = Column(Integer,primary_key=True,index=True)
     english_word = Column(String, index=True)
     language_word_equivalent = Column(String, index=True)
+    description = Column(String, index=True, nullable=True)        
     language_id = Column(Integer,ForeignKey("languages.id"),nullable=True)
     language_fam_id = Column(Integer,ForeignKey("language_families.id"),nullable=True)
-    description = Column(String, index=True, nullable=True)
-    user = Column(Integer,ForeignKey("users.id"),nullable=True)
+    user_id = Column(Integer,ForeignKey("users.id"),nullable=True)
+    creation_date = Column(DateTime, default=datetime.now(), nullable=True)
+    deletion_date = Column(DateTime, nullable=True,)
+
 
     language = relationship("Languages", back_populates="words")
     language_fam = relationship("Language_Families", back_populates="words_fam")
-    
+    user = relationship("users", back_populates="words")
 
 class Languages():
     __tablename__ = 'languages'
@@ -43,10 +48,9 @@ class Languages():
     name = Column(String, index=True)
     language_description = Column(String, index=True, nullable=True)
     language_family_id = Column(Integer,ForeignKey("language_families.id"),nullable=True)
-    creation_date = Column(DateTime,nullable=True)
+    creation_date = Column(DateTime,default=datetime.now(), nullable=True)
     deletion_date = Column(DateTime,nullable=True,)
 
-    
     language_family = relationship("Language_Families", back_populates="lang")
     words = relationship("Words", back_populates="user_lang")
     
@@ -56,9 +60,8 @@ class Language_Families():
     id = Column(Integer,primary_key=True,index=True)
     name = Column(String, index=True)
     language_family_description = Column(String, index=True, nullable=True)
-    creation_date = Column(DateTime, nullable=True,)
+    creation_date = Column(DateTime, default=datetime.now(), nullable=True,)
     deletion_date = Column(DateTime, nullable=True,)
-
     
     words_fam = relationship("Words", back_populates="language_fam")
     lang = relationship("Languages", back_populates="language_family")
@@ -71,14 +74,13 @@ class WordList():
     creation_date = Column(DateTime, nullable=True)
     deletion_date = Column(DateTime, nullable=True)
 
-    picture = image_attachment('WordPicture')
+    picture = image_attachment('WordPicture',nullable=True)
 
 class WordPicture(Base, Image):
     __tablename__ = 'wordpicture'
 
     wordlist_id = Column(Integer,ForeignKey('user.id'),primary_key=True)
-    creation_date = Column(DateTime, nullable=True)
+    creation_date = Column(DateTime, default=datetime.now(), nullable=True)
     deletion_date = Column(DateTime, nullable=True,)
     
     wordlist = relationship('WordList')
-    
