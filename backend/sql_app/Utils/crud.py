9cfecedf66e95f707b7d6d6d532b98bfc4eb2193
Kiwavi <dekamotho@gmail.com>
2024-01-28 13:08:04 +0300

    feat: import email validator and use it

--- backend/sql_app/Utils/crud.py
+++ backend/sql_app/Utils/crud.py
@@ -3,8 +3,16 @@
 from sqlalchemy.orm import Session
 import bcrypt
 import json
+from email_validator import validate_email, EmailNotValidError
 
 from . import models, schemas
+
+def ValidateMail(email):
+    try:
+        emailinfo = validate_email(email, check_deliverability=False)
+        email = emailinfo.normalized
+    except EmailNotValidError as e:
+        print(str(e))
 
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
@@ -12,16 +20,16 @@ def get_user(db: Session, user_id: int):
 
 def create_user(db: Session, user: schemas.UserCreate):
     # util for creating a user. email, username, password
-    salt = bcrypt.gensalt(rounds=12)
-    password_bytes = user.password.encode('utf-8') 
-    hashed_password = bcrypt.hashpw(password_bytes,salt) # postpone due to some error
-    db_user = models.User(email=user.email, hashed_password=hashed_password, username=user.username)
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    # print(type(db_user))
-    return db_user
-
+    if (validate_email(user.email)):
+        salt = bcrypt.gensalt(rounds=12)
+        password_bytes = user.password.encode('utf-8') 
+        hashed_password = bcrypt.hashpw(password_bytes,salt) # postpone due to some error
+        db_user = models.User(email=user.email, hashed_password=hashed_password, username=user.username)
+        db.add(db_user)
+        db.commit()
+        db.refresh(db_user)
+        # print(type(db_user))
+        return db_user
 
 def get_user_by_email(db: Session, email: str):
     # util for fetching user via email
